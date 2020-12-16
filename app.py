56a1fb50ab941d6088ec3c1e9aee3b06bbc72091
souramrakesh <57832253+souramrakesh@users.noreply.github.com>
2020-12-16 22:06:12 +0530

    Yes

--- app.py
+++ app.py
@@ -6,18 +6,23 @@ app =  Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello!"
-@app.route("/sms", methods=['POST']) 
+@app.route("/sms", methods=['POST'])
+
+def mg(msg):
+    if msg == "I love you ":
+        return "I love you too banggaru "
+    else:
+        return msg 
 
 def sms_rly():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
-    If msg == "I love you ":
-       Print ("I love you banggaru ")
+  
 
     # Create reply
     resp = MessagingResponse()
-    resp.message(msg)
+    resp.message(mg)
 
     return str(resp)
 
