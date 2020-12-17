94b3a89fcbec6f6bbc5f5d0400aa19b84a195baa
souramrakesh <57832253+souramrakesh@users.noreply.github.com>
2020-12-17 16:34:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-
+from dialogflow  import fetch_reply
 app =  Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello!"
 @app.route("/sms", methods=['POST'])
-    
-    
+
 def sms_rly():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
+    phone_no = request.form.get('From')
+    reply = fetch_reply(msg,phone_no)
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("Gd to see you:{}".format(msg))
+    resp.message(reply)
 
     return str(resp)
 
