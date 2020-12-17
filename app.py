eec41253374471ae0472522e74aafdd48c42b10b
souramrakesh <57832253+souramrakesh@users.noreply.github.com>
2020-12-17 13:10:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ app =  Flask(__name__)
 def hello():
     return "Hello!"
 @app.route("/sms", methods=['POST'])
-
+def mg(msg):
+    if msg == "hi":
+        return "hlo"
+    else:
+        return msg 
+    
+    
 def sms_rly():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
@@ -15,7 +21,7 @@ def sms_rly():
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    resp.message((mg(msg)))
 
     return str(resp)
 
